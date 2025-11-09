c3284bcc32b64083967d1dc40eea2b89db020f3a
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-11-09 20:06:10 +0700

    Add files via upload

--- Almost/models/Course.py
+++ Almost/models/Course.py
@@ -1,22 +1,17 @@
 import persistent
-from persistent.list import PersistentList
-import globals
-class Course(persistent.Persistent):
-    def __init__(self, id = 0, name = "", file_path = "", curriculum=11, ):
+import bcrypt
+import ZODB, ZODB.FileStorage
+from BTrees.OOBTree import OOBTree
+from persistent import Persistent
+import transaction
+import BTrees.__OOBTree
+
+class Course(persistent.persistent):
+    def __init__(self, id = 0, name = "", professor = 0, file_path = ""):
         self.id = id
         self.name = name
-        self.professor= "Not Assigned"
+        self.professor = professor
         self.file_path = file_path
-        self.enrolled_student = PersistentList()
-        self.curriculum=curriculum
-        
-    
-    def get_quizzes(self):
-        quizzes=[]
-        for quiz in globals.root["quizzes"]:
-            if globals.root["quizzes"][quiz].course_id==self.id:
-                quizzes.append(globals.root["quizzes"][quiz])
-        return quizzes
-                
+        self.enrolled_student = []
 
     
