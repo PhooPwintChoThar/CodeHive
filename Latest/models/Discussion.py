f8c4c1a2661773a8998fbb66e4debedf1d84f058
Thiri Thaw <thiri12505@gmail.com>
2025-11-09 15:00:20 +0700

    Add files via upload

---
+++ Latest/models/Discussion.py
@@ -0,0 +1,25 @@
+import persistent
+import persistent.mapping
+
+class Discussion(persistent.Persistent):
+    def __init__(self, id, student, topic, message, timestamp):
+        self.id = id
+        self.student = student #id
+        self.topic = topic
+        self.message = message
+        self.comment = persistent.mapping.PersistentMapping()
+        self.like_counts = 0
+        self.timestamp = timestamp
+    
+    def create_comment(self, student, content):
+        self.comment[student] = content
+        self._p_changed = True 
+    
+    def like(self):
+        self.like_counts += 1
+        self._p_changed = True
+    
+    def dislike(self):
+        if self.like_counts > 0:
+            self.like_counts -= 1
+        self._p_changed = True
