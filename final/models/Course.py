d06d512d9255ac73f7a319dff3854b4e91c5a1f0
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-11-11 23:44:58 +0700

    Create Course.py

---
+++ final/models/Course.py
@@ -0,0 +1,21 @@
+import persistent
+from persistent.list import PersistentList
+import globals
+class Course(persistent.Persistent):
+    def __init__(self, id = 0, name = "", curriculum=11, ):
+        self.id = id
+        self.name = name
+        self.professor= "Not Assigned"
+        self.enrolled_student = PersistentList()
+        self.curriculum=curriculum
+        
+    
+    def get_quizzes(self):
+        quizzes=[]
+        for quiz in globals.root["quizzes"]:
+            if globals.root["quizzes"][quiz].course_id==self.id:
+                quizzes.append(globals.root["quizzes"][quiz])
+        return quizzes
+                
+
+    
